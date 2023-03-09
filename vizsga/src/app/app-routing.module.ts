5ebdfe1d0c49ec73d6ba343f34ad417a544b1d56
B3nce28 <borsos015@gmail.com>
2023-03-09 21:43:46 +0100

    angular

---
+++ vizsga/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const routes: Routes = [
+  {path: 'login' , component:LoginComponent},
+  {path: 'regist' , component:RegistrationComponent},
+  {path: '' , component:HomePageComponent},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
